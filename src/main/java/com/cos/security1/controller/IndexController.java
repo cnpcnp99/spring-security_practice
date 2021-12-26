159dc31b05863600048d279ac92adac5c885c280
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2021-12-26 20:30:43 +0900

    SecurityConfig 기본 설정

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -10,5 +11,36 @@ public class IndexController {
     @GetMapping({"", "/"}) // localhost:8080/ or localhost:8080
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    // 스프링시큐리티가 해당 주소를 낚아챔 - SecurityConfig 파일 작성 이후에는 작동 안함
+    @GetMapping("/login")
+    public  String login() {
+        return "loginForm";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨";
     }
 }
