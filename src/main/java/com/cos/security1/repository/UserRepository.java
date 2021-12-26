2aa47f792235d06569aa3277e3fd11762f8a90ad
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2021-12-26 22:33:47 +0900

    login시 스프링 시큐리티를 활용해 로그인을 성공시켜줌 + 로그인을 요청했던 페이지로 리다이렉트해줌

--- src/main/java/com/cos/security1/repository/UserRepository.java
+++ src/main/java/com/cos/security1/repository/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //CRUD함수를 JpaRepository를 들고 있음
 // @Repository라는 어노테이션이 없어도
 public interface UserRepository extends JpaRepository<User, Integer> {
+    // findBy는 규칙, Username 부분은 문법
+    // select * from user where username=?
+    public User findByUsername(String username);
 }
